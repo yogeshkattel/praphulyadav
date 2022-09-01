b4fc31bae629941560d0f9cda33d2c2d5205838f
lord <lord@pop-os.localdomain>
2022-09-01 16:24:06 +0545

    schedule fix

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ WORKDIR /home/app
 COPY ./ /home/app
 RUN pip install -r requirements.txt
 RUN python manage.py crontab add
+
 CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
 
 EXPOSE 8000
--- blog/cron.py
+++ blog/cron.py
@@ -6,7 +6,7 @@ def post_schedule():
     posts = Post.objects.all()
     for post in posts:
         # check the date and hour minute of the post and compare with the current time
-        print(post.schedule_time.date(), post.schedule_time.hour, post.schedule_time.minute) 
+        print(post.schedule_time.date() , timezone.now().date() , post.schedule_time.hour == timezone.now().hour , post.schedule_time.minute , timezone.now().minute) 
         if post.schedule_time.date() == timezone.now().date() and post.schedule_time.hour == timezone.now().hour and post.schedule_time.minute == timezone.now().minute:
             post.is_published = True
             post.save()
