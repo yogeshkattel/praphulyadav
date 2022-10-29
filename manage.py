cdd5f2a77f9044d8c4538af4c92e183e59683f2a
Aneesh_001 <69197689+Aneesh-design@users.noreply.github.com>
2022-10-29 21:25:19 +0545

    refactor code + add func desc

--- manage.py
+++ manage.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+
 import os
 import sys
 
-
+# main method
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web.settings')
     try:
